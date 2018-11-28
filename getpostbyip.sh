55dabfa89bb9b96e36612ef351ae0d496b3158d1
Mr La Marne <remi.la-marne@ac-amiens.fr>
2018-11-28 16:14:48 +0100

    ajout de la gestion de l'adresse ip en argument

--- getpostbyip.sh
+++ getpostbyip.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-echo "Hello stranger.";        
-printf "Let us know the arp name you're looking for :";
-read adresseIp;
+echo "Hello stranger.";
+#Checking if ip adresse is supplied
+if [ $# -eq 1 ]
+then
+    ip=$1
+else
+    printf "Let us know the arp name you're looking for :";
+    read adresseIp;
+    ip=adresseIp;
+fi
 
-echo "searching for $adresseIp"
+echo "searching for $ip"
 echo "launching a few pings to fill arp data.";
-#-q pour quiet , -c pour 4 envoie de ping 
-ping -q -c 4 $adresseIp;
+#-q pour quiet , -c pour 4 envoie de ping
+ping -q -c 4 $ip;
 
 printf "Showing arp infos : \e[92m";
-arp -a | grep -F "$adresseIp" 
-
-
+arp -a | grep -F "$ip"
